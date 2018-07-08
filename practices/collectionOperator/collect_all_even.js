98c00f7e49d1b4405221661e7c9da79fc6ac9607
Cisnoc Xie <502021156@qq.com>
2018-07-08 17:25:25 +0800

    modify package.json to success running testing and pass collect_all_even_spec.js

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  let result = [];
+  for (let i of collection) {
+    if (i % 2 === 0) {
+      result.push(i);
+    }
+  }
+  return result;
 }
 
 module.exports = collect_all_even;
