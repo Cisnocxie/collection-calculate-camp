e3056f625c0a95febf0a15fc7caec8886402ec3b
Cisnoc Xie <502021156@qq.com>
2018-07-08 17:46:26 +0800

    pass get_integer_interval_2_spec.js

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,24 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  let result = [];
+  if (number_a < number_b) {
+    if (number_a % 2 === 0) {
+      result.push(number_a);
+    } else {
+      result.push(number_a + 1);
+    }
+    while (result[result.length - 1] + 2 <= number_b) {
+      result.push(result[result.length - 1] + 2);
+    }
+  } else if (number_a > number_b) {
+    result = get_integer_interval_2(number_b, number_a).reverse();
+  } else {
+    if (number_a % 2 === 0) {
+      result.push(number_a);
+    }
+  }
+  return result;
 }
 
 module.exports = get_integer_interval_2;
