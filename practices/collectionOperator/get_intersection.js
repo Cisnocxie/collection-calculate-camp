97819b5b0fb5a14dc346395aa7ea717fa13bb50d
Cisnoc Xie <502021156@qq.com>
2018-07-08 17:56:54 +0800

    pass get_intersection_spec.js

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+  let result = [];
+  for (let i of collection_b) {
+    for (let j of collection_a) {
+      if (i === j) {
+        result.push(i);
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = get_intersection;
