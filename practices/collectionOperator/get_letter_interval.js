50616ecec0f0dc0db0cb246c4f0bfb9f42b1cb59
Cisnoc Xie <502021156@qq.com>
2018-07-08 18:10:49 +0800

    pass get_letter_interval_spec.js

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -1,7 +1,14 @@
 'use strict';
 
+let get_integer_interval = require('./get_integer_interval.js');
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+              'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let result = [];
+  for (let i of get_integer_interval(number_a, number_b)) {
+    result.push(alphabet[i - 1]);
+  }
+  return result;
 }
 
 module.exports = get_letter_interval;
